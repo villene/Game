e96b76c1cfce12fcf6240c46af46423f9fa1a64b
Guntis <guntis.smaukstelis@gmail.com>
2014-03-16 15:36:44 +0200

    added info to menu screen

--- js/ui/menu.js
+++ js/ui/menu.js
@@ -13,15 +13,23 @@ var Menu = Class.extend({
         var bg = this.group.create(game.width/2, game.height/2, 'popup_bg');
         bg.anchor.setTo(0.5, 0.5);
 
+        var heading = game.add.text(game.width/2, 60, 'FortaNota', {font: "50px Arial", fill: "#fff"});
+        heading.anchor.setTo(0.5, 0);
+        this.group.add(heading);
+
         // scoresList
         var scoreX = 280;
-        var scoreY = 150;
+        var scoreY = 180;
         var lineHeight = 30;
+        var description = game.add.text(scoreX, scoreY, 'Song: '+songData.name, { font: "20px Arial", fill: "#333" });
+        this.group.add(description);
 //        this.scores = new List(scoreX, scoreY, lineHeight, CONFIG.scores);
 //        this.group.add( this.scores.group );
 
         // octavesList
         var scoreX = 530;
+        var octaveText = game.add.text(scoreX, scoreY-30, 'Select Octave', { font: "20px Arial", fill: "#333" });
+        this.group.add( octaveText );
         var octavesList = [{title:2}, {title:3}, {title: 4}, {title: 5}];
         this.octaves = new List(scoreX, scoreY, lineHeight, octavesList, this.defaultOctave);
         this.group.add( this.octaves.group );
