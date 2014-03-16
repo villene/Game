8cdd8407b9f0a6e5bde22544cad05a57ce4c4bb4
Guntis <guntis.smaukstelis@gmail.com>
2014-03-17 01:12:44 +0200

    find max and min midi

--- js/songData.js
+++ js/songData.js
@@ -6,6 +6,10 @@ var SongData = Class.extend({
         this.octaveData = this.getOctaveData(this.xml);
         this.octave = this.getCommonOctave(this.octaveData);
 
+        this.minMidi;
+        this.maxMidi;
+        this.findMaxMinMidi(this.xml);
+
         if (!this.xml) console.log('ERROR! XML file does not exist!');
     }, getOctaveData: function (xml) {
         var octaveList = [0, 0, 0, 0, 0, 0, 0, 0];
@@ -30,7 +34,6 @@ var SongData = Class.extend({
         }
         return octaveList;
     }, getCommonOctave: function (arr) {
-        // TODO iterate through array and get most used octave
         var maxVal = 0;
         var maxIterator = false;
         for (var i = 0, l = arr.length; i < l; i++) {
@@ -40,7 +43,52 @@ var SongData = Class.extend({
             }
         }
         return maxIterator;
-    }, parseUrl: function () {
+    }
+
+    , findMaxMinMidi: function(xml){
+        this.minMidi = false;
+        this.maxMidi = false;
+        var notes = xml.getElementsByTagName("note");
+
+        for (var i = 0, l = notes.length; i < l; i++) {
+            if (notes[i].getElementsByTagName("step")[0]) {
+                var step = notes[i].getElementsByTagName("step")[0].childNodes[0].nodeValue;
+            } else {
+                var step = false;
+            }
+            if (notes[i].getElementsByTagName("octave")[0]) {
+                var octave = notes[i].getElementsByTagName("octave")[0].childNodes[0].nodeValue - 1;
+            } else {
+                var octave = false;
+            }
+            if (notes[i].getElementsByTagName("alter")[0] && notes[i].getElementsByTagName("alter")[0].childNodes[0]) {
+                var alter = notes[i].getElementsByTagName("alter")[0].childNodes[0].nodeValue;
+            } else {
+                var alter = false;
+            }
+
+            if(!octave || !step || !alter){
+                continue; // check against rest elements
+            }
+
+            var freq = this.noteToFrequency(octave, step, alter);
+            freq = this.frequencyToNote(freq);
+            var midi = freq.midi;
+
+            if(!this.minMidi || this.minMidi > midi){
+                this.minMidi = midi;
+            }
+
+            if(!this.maxMidi || this.maxMidi < midi){
+                this.maxMidi = midi;
+            }
+        }
+
+        this.minMidi = Math.round(this.minMidi);
+        this.maxMidi = Math.round(this.maxMidi);
+    }
+
+    , parseUrl: function () {
         var url = {};
         var tmp = location.search.substring(1);
         tmp = tmp.split('&');
@@ -55,5 +103,140 @@ var SongData = Class.extend({
         xmlhttp.send();
         xmlDoc = xmlhttp.responseXML;
         return xmlDoc;
+    }
+
+    , noteToFrequency: function (oct, step, alter) {
+        var freq;
+        switch (step) {
+            case 'C':
+                step = oct * 12 + 1;
+                break;
+            case 'C♯':
+                step = oct * 12 + 2;
+                break;
+            case 'D':
+                step = oct * 12 + 3;
+                break;
+            case 'D♯':
+                step = oct * 12 + 4;
+                break;
+            case 'E':
+                step = oct * 12 + 5;
+                break;
+            case 'F':
+                step = oct * 12 + 6;
+                break;
+            case 'F♯':
+                step = oct * 12 + 7;
+                break;
+            case 'G':
+                step = oct * 12 + 8;
+                break;
+            case 'G♯':
+                step = oct * 12 + 9;
+                break;
+            case 'A':
+                step = oct * 12 + 10;
+                break;
+            case 'A♯':
+                step = oct * 12 + 11;
+                break;
+            case 'B':
+                step = oct * 12 + 12;
+                break;
+        }
+        if (alter === '1')step += 1;
+        else if (alter === '-1')step -= 1;
+
+        if (step === 58) freq = 440;
+        else {
+            step -= 58;
+            freq = 440 * Math.pow(1.059463, step);
+        }
+
+        return freq.toFixed(2);
+    }, frequencyToNote: function (freq) {
+        var note;
+        var oct;
+        var step;
+        var diff = 12 * Math.log(freq / 440) / Math.log(2);
+        diff = Math.round(diff);
+
+        note = 58 + diff;
+        oct = Math.floor(note / 12);
+        note %= 12;
+
+        switch (note) {
+            case 1:
+            {
+                step = 'C';
+                note = 1;
+                break;
+            }
+            case 2:
+            {
+                step = 'C♯';
+                break;
+            }
+            case 3:
+            {
+                step = 'D';
+                note = 2;
+                break;
+            }
+            case 4:
+            {
+                step = 'D♯';
+                break;
+            }
+            case 5:
+            {
+                step = 'E';
+                note = 3;
+                break;
+            }
+            case 6:
+            {
+                step = 'F';
+                note = 4;
+                break;
+            }
+            case 7:
+            {
+                step = 'F♯';
+                break;
+            }
+            case 8:
+            {
+                step = 'G';
+                note = 5;
+                break;
+            }
+            case 9:
+            {
+                step = 'G♯';
+                break;
+            }
+            case 10:
+            {
+                step = 'A';
+                note = 6;
+                break;
+            }
+            case 11:
+            {
+                step = 'A♯';
+                break;
+            }
+            case 0:
+            {
+                step = 'B';
+                oct--;
+                note = 7;
+                break;
+            }
+        }
+        var midi = 12 * (Math.log(freq / 440) / Math.log(2)) + 69;
+        return {step: step, oct: oct, note: note, string: step + oct, midi: midi};
     }
 })
